0579ca06b796bfb2e218c3f45a6cc279fd22f1a9
julio cesar khichfy <julio.khichfy@mj.gov.br>
2023-06-28 12:31:47 -0300

    início

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+          allowedUrls: ['http://localhost:8080/foo'],
+          sendAccessToken: true
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
