277d88d4e216f2a05dda682970e25d5ef1053737
julio cesar khichfy <julio.khichfy@mj.gov.br>
2023-06-30 12:46:49 -0300

    update

--- src/app/foo/update/update.component.ts
+++ src/app/foo/update/update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Foo } from 'src/app/model/foo';
+import { FooService } from 'src/app/services/foo.service';
 
 @Component({
   selector: 'app-update',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fooService:FooService,
+    private activedRoute: ActivatedRoute,
+    private router: Router) { }
 
+  foo: Foo;
+  
   ngOnInit(): void {
+
+    const id = this.activedRoute.snapshot.params.id;
+    this.fooService.detail(id).subscribe((data: Foo) => {
+      this.foo = data;
+    },
+    err =>{
+      console.log(err);
+    })
+  }
+
+  onUpdate(): void {
+    const id = this.activedRoute.snapshot.params.id;
+    this.fooService.update(id, this.foo).subscribe((data)=>{
+      console.log(data);
+      this.voltar();
+    }, err => {
+      console.log(err)
+    })
+  }
+
+  voltar(){
+    this.router.navigate(['/lista']);
   }
 
 }
